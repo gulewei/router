beae8a73131d1b653198912da277dc411553fb97
gulewei <gu.lewei@qq.com>
2018-09-10 15:44:24 +0800

    umd build

--- dist/hoa-router.js
+++ dist/hoa-router.js
@@ -189,8 +189,10 @@ function sessionFactory(history) {
           switch (action) {
             case 'PUSH':
               nextStack = stack.concat(location);
+              break;
             case 'REPLACE':
               nextStack = stack.slice(0, -1).concat(location);
+              break;
             case 'POP':
             default:
               nextStack = stack;
@@ -241,6 +243,133 @@ function withRouter(app, _ref) {
     };
 }
 
+var asyncGenerator = function () {
+  function AwaitValue(value) {
+    this.value = value;
+  }
+
+  function AsyncGenerator(gen) {
+    var front, back;
+
+    function send(key, arg) {
+      return new Promise(function (resolve, reject) {
+        var request = {
+          key: key,
+          arg: arg,
+          resolve: resolve,
+          reject: reject,
+          next: null
+        };
+
+        if (back) {
+          back = back.next = request;
+        } else {
+          front = back = request;
+          resume(key, arg);
+        }
+      });
+    }
+
+    function resume(key, arg) {
+      try {
+        var result = gen[key](arg);
+        var value = result.value;
+
+        if (value instanceof AwaitValue) {
+          Promise.resolve(value.value).then(function (arg) {
+            resume("next", arg);
+          }, function (arg) {
+            resume("throw", arg);
+          });
+        } else {
+          settle(result.done ? "return" : "normal", result.value);
+        }
+      } catch (err) {
+        settle("throw", err);
+      }
+    }
+
+    function settle(type, value) {
+      switch (type) {
+        case "return":
+          front.resolve({
+            value: value,
+            done: true
+          });
+          break;
+
+        case "throw":
+          front.reject(value);
+          break;
+
+        default:
+          front.resolve({
+            value: value,
+            done: false
+          });
+          break;
+      }
+
+      front = front.next;
+
+      if (front) {
+        resume(front.key, front.arg);
+      } else {
+        back = null;
+      }
+    }
+
+    this._invoke = send;
+
+    if (typeof gen.return !== "function") {
+      this.return = undefined;
+    }
+  }
+
+  if (typeof Symbol === "function" && Symbol.asyncIterator) {
+    AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
+      return this;
+    };
+  }
+
+  AsyncGenerator.prototype.next = function (arg) {
+    return this._invoke("next", arg);
+  };
+
+  AsyncGenerator.prototype.throw = function (arg) {
+    return this._invoke("throw", arg);
+  };
+
+  AsyncGenerator.prototype.return = function (arg) {
+    return this._invoke("return", arg);
+  };
+
+  return {
+    wrap: function (fn) {
+      return function () {
+        return new AsyncGenerator(fn.apply(this, arguments));
+      };
+    },
+    await: function (value) {
+      return new AwaitValue(value);
+    }
+  };
+}();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var _extends = Object.assign || function (target) {
   for (var i = 1; i < arguments.length; i++) {
     var source = arguments[i];
@@ -257,7 +386,7 @@ var _extends = Object.assign || function (target) {
 
 var NAME$1 = 'router';
 
-function withRouter$2(app, _ref) {
+function withSession(app, _ref) {
     var _ref$name = _ref.name,
         name = _ref$name === undefined ? NAME$1 : _ref$name,
         history = _ref.history;
@@ -288,7 +417,7 @@ exports.routerFactory = routerFactory;
 exports.sessionFactory = sessionFactory;
 exports.Switch = Switch;
 exports.withRouter = withRouter;
-exports.withSession = withRouter$2;
+exports.withSession = withSession;
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
